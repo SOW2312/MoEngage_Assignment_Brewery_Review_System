f124ccd1bb84fdfd6811a5b84b3b4e685f8215ff
ANJALI UPADHYAYA <80103375+Anjali-Upadhyaya@users.noreply.github.com>
2023-11-08 23:07:10 +0530

    Python APIs

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
-# Set secret key for message flashing
+# secret key for message flashing
 app.secret_key = os.urandom(24)
 
 
 def send_password_to_email(to_address, password):
-    # Set up the SMTP server
+    # the SMTP server
     smtp_server = "your_smtp_server.com"
     smtp_port = 587
-    username = "abcdsfhan@gmail.com"
+    username = "aupadhya2001@gmail.com"
     password = "zpaf xboz emoh upf"
-    from_address = "abcdsfhan@gmail.com"
+    from_address = "aupadhya2001@gmail.com"
 
-    # Create the email message
+    # the email message
     subject = "Your Password Recovery"
     body = f"Your password is: {password}"
 
@@ -30,15 +30,14 @@ def send_password_to_email(to_address, password):
     msg["To"] = to_address
 
     try:
-        # Connect to the SMTP server
+        # Connecting to the SMTP server
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(username, password)
 
-        # Send the email
+        # Send email
         server.sendmail(from_address, [to_address], msg.as_string())
 
-        # Close the connection
         server.quit()
 
         return True  # Email sent successfully
@@ -48,28 +47,28 @@ def send_password_to_email(to_address, password):
         return False  # Failed to send email
 
 
-# Connect to the MongoDB database
+# Connecting to the MongoDB database
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["brewery_review_system"]
 
-# Create a collection in the database to store reviews
+# collection in the database to store reviews
 reviews_collection = db["reviews"]
 
 users_collection = db["users"] 
 
-# Define the index route
+# the index route
 @app.route("/",methods=["GET", "POST"])
 def index():
      return render_template("home.html")
 
-# Define the login route
+# the login route
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
 
-        # Check if the user exists in the database
+        # the user exists in the database
         user = users_collection.find_one({"username": username})
         if user is not None and user["password"] == password:
             # Login successful
@@ -81,20 +80,21 @@ def login():
     else:
         return render_template("login.html")
 
+#forgot email
 @app.route("/forgot_password", methods=["GET", "POST"])
 def forgot_password():
     if request.method == "POST":
         username = request.form["username"]
 
-        # Check if the user with the provided username exists in the database
+        # Checking if the user with the provided username exists in the database
         user = users_collection.find_one({"username": username})
         if user is not None:
-            # Check if the provided username matches the registered email address
+            # Checking if the provided username matches the registered email address
             if user["username"] == user:
                 # Send the password to the user's email
                 send_password_to_email(user["username"], user["password"])
 
-                # Provide a success message (you may redirect or display a success message)
+                # Provide a success message 
                 flash("Password sent to your email.", "success")
                 return redirect(url_for("login"))
             else:
@@ -108,7 +108,7 @@ def forgot_password():
     else:
         return render_template("forgot_password.html")
 
-# Define the signup route
+# signup route
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -116,18 +116,17 @@ def signup():
         password = request.form["password"]
 
         if len(password) < 8:
-            # Password is too short, show an error message
             error_message = "Password must be at least 8 characters long"
             return render_template("signup.html", error_message=error_message)
         
-        # Check if the username already exists
+        # Checking if the username already exists
         user = users_collection.find_one({"username": username})
         if user is not None:
             # Username already exists
             error_message = "Username already taken"
             return render_template("signup.html", error_message=error_message)
         else:
-            # Create a new user in the database
+            # Creating new user in the database
             users_collection.insert_one({"username": username, "password": password})
 
             # Signup successful
@@ -136,7 +135,7 @@ def signup():
         return render_template("signup.html")
 
 
-
+#search
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
@@ -152,7 +151,6 @@ def search():
 
         breweries = response.json()
 
-        # Extract required details from the response
         formatted_breweries = []
         for brewery in breweries:
             formatted_brewery = {
@@ -171,12 +169,12 @@ def search():
         return render_template("search.html")
 
 
-# Define the brewery route
+#rating
 @app.route("/brewery/<name>", methods=["GET", "POST"])
 def brewery(name):
     brewery = requests.get(f"https://api.openbrewerydb.org/breweries?by_name={name}").json()
 
-    # Get reviews for the brewery from the database
+    # Geting reviews for the brewery from the database
     brewery_reviews = reviews_collection.find({"brewery_name": name})
 
     if request.method == "POST":
@@ -187,18 +185,17 @@ def brewery(name):
         review = { "brewery_name": name ,"rating": rating, "description": description}
         reviews_collection.insert_one(review)
         
-         # Flash a success message
         flash("Review and Rating submitted successfully!", "success")
 
 
     return render_template("brewery.html", brewery=brewery, reviews=brewery_reviews)
 
-# Define the brewery info route
+# brewery info route
 @app.route("/brewery_info/<name>", methods=["GET"])
 def brewery_info(name):
     brewery = requests.get(f"https://api.openbrewerydb.org/breweries?by_name={name}").json()
 
-    # Get reviews for the brewery from the database
+    # Geting reviews for the brewery from the database
     brewery_reviews = reviews_collection.find({"brewery_name": name})
 
     return render_template("brewery_info.html", brewery=brewery, reviews=brewery_reviews)
